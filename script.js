0dcb3ed53f689d1d2cc616b3859d79c88df516ba
Felipe Mendes <felipemadruga@gmail.com>
2022-11-16 10:40:40 -0300

    função potencia

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function passandoPelosPares() {
   for (let i = 2; i <= 6; i += 2)
     console.log ('aqui eu tenho o valor de ' + i)
   }
-  
-  passandoPelosPares();
 
+passandoPelosPares();
+
+const power = (a, b) => {
+ return a ** b
+}
+console.log(power(3, 2));  
 
 const soma = (num1, num2) => {
     console.log(num1 + num2)
